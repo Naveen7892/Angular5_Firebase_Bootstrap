c2675c424d40bd421f4d0f7983f9d2ba32df3e2f
Naveen Kumar <metanaveen@outlook.com>
2018-02-23 21:40:17 +0530

    Angular5 - Bootstrap starter template - Firebase DB - Tutorial completed

---
+++ ng4fbbootstrap/src/app/app-navbar/app-navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-app-navbar',
+  selector: 'app-navbar',
+  templateUrl: './app-navbar.component.html',
+  styleUrls: ['./app-navbar.component.css']
+})
+export class AppNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ng4fbbootstrap/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCEXe6hCEinUpCi9FLbUYsmyNkg1613Iv4',
+    authDomain: 'ng4fbbootstrap-f48e0.firebaseapp.com',
+    databaseURL: 'https://ng4fbbootstrap-f48e0.firebaseio.com',
+    projectId: 'ng4fbbootstrap-f48e0',
+    storageBucket: '',
+    messagingSenderId: '25096873965'
+  }
+};
